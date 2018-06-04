3e705142878d8ba082c621a782d4666554272c4a
Flore Larretchart <florenciage@gmail.com>
2018-06-04 09:28:28 -0300

    Prime version del proyecto Mascotas

---
+++ src/wolfbits/Pet.java
@@ -0,0 +1,22 @@
+
+package wolfbits;
+
+import java.io.File;
+import java.util.List;
+
+public class Pet {
+    private String name;
+    private List<String> razas;
+    private int tipoAnimal;// 0=perro o 1=gato
+    private List<String> colores;
+    private String tamaño;
+    private String sexo;
+    private String descripcion;
+    private int aging; // 0=cachorro, 1=joven, 2=adulto
+    private String eyeColor;//
+    private String hairType; //
+    private File imagen;
+    
+}
+//contructores:mandatorios: razas, tipoAnimal,colores,sexo, tamaño,
+//constructor con todo los atributos
